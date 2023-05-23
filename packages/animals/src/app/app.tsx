349a5ca93f5b538ca48dfc698ebbecb5ab893f86
Thiago Martins <thiago_valentim1@hotmail.com>
2023-05-23 19:12:34 -0300

    feat(animals): add button with event handler

--- packages/animals/src/app/app.tsx
+++ packages/animals/src/app/app.tsx
@@ -2,9 +2,14 @@
 import styles from './app.module.css';
 
 export function App() {
+  const handleClick = () => {
+    console.log('Button was clicked!');
+  };
+
   return (
     <div>
       <p>Hello</p>
+      <button onClick={handleClick}>Click Me</button>
     </div>
   );
 }
