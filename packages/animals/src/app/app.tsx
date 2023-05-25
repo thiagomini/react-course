c798df82f63f46f2afabd870e2a05d17b4b5f06d
Thiago Martins <thiago_valentim1@hotmail.com>
2023-05-25 18:36:12 -0300

    feat(animals): add click handler and button

--- packages/animals/src/app/app.tsx
+++ packages/animals/src/app/app.tsx
@@ -1,15 +1,20 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { useState } from 'react';
 
 export function App() {
+  const increase = (num: number) => num + 1;
   const handleClick = () => {
-    console.log('Button was clicked!');
+    setCount(increase);
   };
+
+  const [count, setCount] = useState(0);
 
   return (
     <div>
       <p>Hello</p>
-      <button onClick={handleClick}>Click Me</button>
+      <button onClick={handleClick}>Add Animal</button>
+      <div>Number of animals: {count}</div>
     </div>
   );
 }
