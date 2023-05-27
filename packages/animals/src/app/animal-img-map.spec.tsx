85a0cf396acde5bf55a756975a4fa7aef122fbb4
Thiago Martins <thiago_valentim1@hotmail.com>
2023-05-27 11:17:21 -0300

    feat(animals): add all animals svg maps

--- packages/animals/src/app/animal-img-map.spec.tsx
+++ packages/animals/src/app/animal-img-map.spec.tsx
@@ -1,4 +1,8 @@
 import birdPath from '../assets/bird.svg';
+import catPath from '../assets/cat.svg';
+import cowPath from '../assets/cow.svg';
+import gatorPath from '../assets/gator.svg';
+import horsePath from '../assets/horse.svg';
 
 describe('Animal image map', () => {
   test('returns bird path', () => {
@@ -6,10 +10,42 @@ describe('Animal image map', () => {
 
     expect(bird).toEqual(birdPath);
   });
+
+  test('returns cat path', () => {
+    const cat = imageForAnimal('cat');
+
+    expect(cat).toEqual(catPath);
+  });
+
+  test('returns cow path', () => {
+    const cow = imageForAnimal('cow');
+
+    expect(cow).toEqual(cowPath);
+  });
+
+  test('returns gator path', () => {
+    const gator = imageForAnimal('gator');
+
+    expect(gator).toEqual(gatorPath);
+  });
+
+  test('returns horse path', () => {
+    const horse = imageForAnimal('horse');
+
+    expect(horse).toEqual(horsePath);
+  });
 });
 
 type Animal = 'cat' | 'dog' | 'cow' | 'gator' | 'horse' | 'bird';
 
 function imageForAnimal(animal: Animal) {
-  return birdPath;
+  const svgMap: Record<string, string> = {
+    bird: birdPath,
+    cat: catPath,
+    cow: cowPath,
+    gator: gatorPath,
+    horse: horsePath,
+  };
+
+  return svgMap[animal];
 }
