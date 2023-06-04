1e48b38c9ab41e64e934f491bbf98b6f5ed27d05
Thiago Martins <thiago_valentim1@hotmail.com>
2023-06-04 12:46:38 -0300

    feat(books): book create component

---
+++ packages/books/src/components/BookCreate.spec.tsx
@@ -0,0 +1,36 @@
+import { render } from '@testing-library/react';
+import { Book, createBook } from '../domain/book';
+import BookCreate from './BookCreate';
+import userEvent from '@testing-library/user-event';
+
+describe('BookCreate', () => {
+  test('renders', () => {
+    const { baseElement } = makeComponent();
+
+    expect(baseElement).toBeInTheDocument();
+  });
+
+  test('create new book on submit', async () => {
+    // Arrange
+    const handleSubmit = jest.fn();
+    const { getByRole } = makeComponent(handleSubmit);
+    const input = getByRole('textbox');
+    await userEvent.type(input, 'New Book Title');
+
+    // Act
+    await userEvent.keyboard('{enter}');
+
+    // Assert
+    expect(handleSubmit).toHaveBeenCalledWith('New Book Title');
+  });
+});
+
+function makeComponent(
+  handleSubmit: (title: string) => Book = makeStubHandleSubmit()
+) {
+  return render(<BookCreate handleSubmit={handleSubmit} />);
+}
+
+function makeStubHandleSubmit() {
+  return () => createBook('title');
+}
--- packages/books/src/components/BookCreate.tsx
+++ packages/books/src/components/BookCreate.tsx
@@ -1,5 +1,30 @@
-function BookCreate() {
-  return <div>Book Create</div>;
+import { useState } from 'react';
+import { Book } from '../domain/book';
+
+export type BookCreateProps = {
+  handleSubmit: (title: string) => Book;
+};
+
+function BookCreate({ handleSubmit }: BookCreateProps) {
+  const [title, setTitle] = useState('');
+
+  const onSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    handleSubmit(title);
+  };
+
+  return (
+    <div>
+      <form onSubmit={onSubmit}>
+        <label>Enter Book Title</label>
+        <input
+          type="text"
+          value={title}
+          onChange={(event) => setTitle(event.target.value)}
+        />
+      </form>
+    </div>
+  );
 }
 
 export default BookCreate;
