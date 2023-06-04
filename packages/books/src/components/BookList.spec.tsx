6d5c0667cdf6beb53063128b5647d6fa2e5ede44
Thiago Martins <thiago_valentim1@hotmail.com>
2023-06-04 17:59:38 -0300

    feat(books): implement book list

---
+++ packages/books/src/components/BookList.spec.tsx
@@ -0,0 +1,21 @@
+import { render } from '@testing-library/react';
+import BookList from './BookList';
+import { Book, createBook } from '../domain/book';
+
+describe('Book List', () => {
+  test('renders a list of books', () => {
+    // Arrange
+    const books = [createBook('Title 1'), createBook('Title 2')];
+
+    // Act
+    const { getByText } = makeComponent(books);
+
+    // Assert
+    expect(getByText('Title 1')).toBeInTheDocument();
+    expect(getByText('Title 2')).toBeInTheDocument();
+  });
+});
+
+function makeComponent(books: ReadonlyArray<Book>) {
+  return render(<BookList books={books} />);
+}
--- packages/books/src/components/BookList.tsx
+++ packages/books/src/components/BookList.tsx
@@ -1,5 +1,15 @@
-function BookList() {
-  return <div>Book List</div>;
+import { Book } from '../domain/book';
+
+export type BookListProps = {
+  books: ReadonlyArray<Book>;
+};
+
+function BookList({ books }: BookListProps) {
+  const renderedBooks = books.map((book) => (
+    <div key={book.id}>{book.title}</div>
+  ));
+
+  return <div>{renderedBooks}</div>;
 }
 
 export default BookList;
