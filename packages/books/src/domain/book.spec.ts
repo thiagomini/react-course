ababa22dd9defbd054095afebd92c3a55a0514c8
Thiago Martins <thiago_valentim1@hotmail.com>
2023-06-04 12:25:33 -0300

    feat(books): create book function

--- packages/books/src/domain/book.spec.ts
+++ packages/books/src/domain/book.spec.ts
@@ -1,6 +1,28 @@
-import { Book, updateTitle } from './book';
+import { Book, createBook, updateTitle } from './book';
 
 describe('Books', () => {
+  test('create a book', () => {
+    // Act
+    const newBook = createBook('The Hobbit');
+
+    // Assert
+    expect(newBook).toEqual({
+      id: expect.any(String),
+      title: 'The Hobbit',
+    });
+  });
+
+  test('create a book with unique id', () => {
+    // Arrange
+    const firstBook = createBook('The Hobbit');
+
+    // Act
+    const secondBook = createBook('The Lord of the Rings');
+
+    // Assert
+    expect(firstBook.id).not.toBe(secondBook.id);
+  });
+
   test('update a book title', () => {
     // Arrange
     const book: Book = { id: '1', title: 'The Hobbit' };
--- packages/books/src/domain/book.ts
+++ packages/books/src/domain/book.ts
@@ -1,7 +1,14 @@
+import { randomUUID } from 'crypto';
+
 export type Book = {
   id: string;
   title: string;
 };
+
+export const createBook = (title: string): Book => ({
+  id: randomUUID(),
+  title,
+});
 
 export const updateTitle = (book: Book, title: string): Book => ({
   ...book,
