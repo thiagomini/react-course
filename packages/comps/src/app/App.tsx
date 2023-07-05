3fafee571306ed3be283ca7f95614979dc68e16c
Thiago Martins <thiago_valentim1@hotmail.com>
2023-07-05 09:37:50 -0300

    feat(comps): create app

---
+++ packages/comps/src/app/App.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './App.module.css';
+
+export function App() {
+  return (
+    <div>
+      <h1>
+        <span> Hello there, </span>
+        Welcome comps 👋
+      </h1>
+    </div>
+  );
+}
+
+export default App;
