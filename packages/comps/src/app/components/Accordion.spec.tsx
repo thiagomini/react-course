3d42d347ae83b15b51d3ffc5301d44689909cae3
Thiago Martins <thiago_valentim1@hotmail.com>
2023-07-15 16:46:50 -0300

    test(comps): ensure all accordion items are displayed

--- packages/comps/src/app/components/Accordion.spec.tsx
+++ packages/comps/src/app/components/Accordion.spec.tsx
@@ -15,6 +15,24 @@ describe('Accordion', () => {
     expect(queryByText('Title 1')).toBeInTheDocument();
     expect(queryByText('Content 1')).not.toBeInTheDocument();
   });
+
+  test('displays many items', () => {
+    const { queryByText } = createComponent({
+      items: [
+        {
+          title: 'Title 1',
+          content: 'Content 1',
+        },
+        {
+          title: 'Title 2',
+          content: 'Content 2',
+        },
+      ],
+    });
+
+    expect(queryByText('Title 1')).toBeInTheDocument();
+    expect(queryByText('Title 2')).toBeInTheDocument();
+  });
 });
 
 function createComponent(props: Partial<AccordionProps>) {
