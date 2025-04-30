583cf3d4fec6c1659b5b78634cdc367f554d9b46
Thiago Martins <thiago_valentim1@hotmail.com>
2025-04-30 09:51:29 -0300

    style(components): enhance Dropdown component layout and styling

--- packages/comps/src/app/components/Dropdown.tsx
+++ packages/comps/src/app/components/Dropdown.tsx
@@ -12,9 +12,16 @@ function Dropdown({ options, value, onChange: onSelect }: DropdownProps) {
     onSelect?.(event.target.value);
   };
   return (
-    <div>
-      <label htmlFor="dropdown">Select</label>
-      <select id="dropdown" value={value} onChange={handleSelectChange}>
+    <div className="flex flex-col space-y-1 max-w-[20%]">
+      <label htmlFor="dropdown" className="text-xl font-medium text-gray-700">
+        Select
+      </label>
+      <select
+        id="dropdown"
+        value={value}
+        onChange={handleSelectChange}
+        className="cursor-pointer border border-gray-300 rounded-md p-2 text-sm focus:outline-none focus:ring-2 focus:ring-blue-500 focus:border-blue-500"
+      >
         {options.map((option) => (
           <option key={option.value} value={option.value}>
             {option.label}
