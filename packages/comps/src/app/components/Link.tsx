b00f9b4fa0293a284d3242336482ba1f8c8d90ff
Thiago Martins <thiago_valentim1@hotmail.com>
2025-05-04 10:27:23 -0300

    style(comps): add style to link component

--- packages/comps/src/app/components/Link.tsx
+++ packages/comps/src/app/components/Link.tsx
@@ -1,11 +1,15 @@
 import { FC, MouseEventHandler, PropsWithChildren } from 'react';
 import useNavigation from '../../hooks/use-navigation-context.hooks';
+import classNames from 'classnames';
 interface LinkProps extends PropsWithChildren {
   to: string;
 }
 
 const Link: FC<LinkProps> = ({ to, children }) => {
   const { navigate } = useNavigation();
+
+  const classes = classNames('text-blue-500');
+
   const handleClick: MouseEventHandler<HTMLAnchorElement> = (event) => {
     if (event.ctrlKey || event.metaKey) {
       return; // Ensures the user can open the link in a new tab while holding CTRL or CMD key.
@@ -15,7 +19,7 @@ const Link: FC<LinkProps> = ({ to, children }) => {
   };
 
   return (
-    <a onClick={handleClick} href={to}>
+    <a onClick={handleClick} href={to} className={classes}>
       {children}
     </a>
   );
